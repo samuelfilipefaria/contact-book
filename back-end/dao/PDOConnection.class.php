c1b9cdaee7d8e3efa19c89642b5ac07a87a22f0d
Samuel Filipe Faria <samuel.filipe@prevision.com.br>
2023-08-20 20:07:49 -0300

    feat(app): Adding missing functions in PDO connection class

--- back-end/dao/PDOConnection.class.php
+++ back-end/dao/PDOConnection.class.php
@@ -1,39 +1,55 @@
 <?php
-    include_once "../config/DBConfig.class.php";
+  include_once "../config/DBConfig.class.php";
 
-    class PDOConnection {
-        private $connection;
+  if (!function_exists('str_starts_with')) {
+    function str_starts_with($haystack, $needle) {
+      return (string)$needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;
+    }
+  }
+  if (!function_exists('str_ends_with')) {
+    function str_ends_with($haystack, $needle) {
+      return $needle !== '' && substr($haystack, -strlen($needle)) === (string)$needle;
+    }
+  }
+  if (!function_exists('str_contains')) {
+    function str_contains($haystack, $needle) {
+      return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+    }
+  }
 
-        public function connect() {
-            try {
-                $this->connection = new PDO(
-                    "mysql:host=".DBConfig::$HOST.";dbname=".DBConfig::$DB, DBConfig::$USER,
-                    DBConfig::$PWD);
-            } catch (Exception $ex) {
-                throw new Exception('Erro ao tentar se conectar.
-                        Mensagem: '.$ex->getMessage());
-            }
-        }
+  class PDOConnection {
+    private $connection;
 
-        public function prepareStm($query, $fields) {
-            $stm = $this->connection->prepare($query);
-            foreach($fields as $key => &$value) {
-                $stm->bindParam(":".$key, $value);
-            }
-            return $stm;
-        }
-
-        public function executeQuery($stm) {
-            $isSelect = str_starts_with(strtolower($stm->queryString), "select");
-            $stm->execute();
-            $arr = $stm->fetchAll(PDO::FETCH_ASSOC);
-            $numRows = $stm->rowCount();
-            $stm = null;
-            if (!$isSelect)
-                return $numRows;
-
-            return $arr;
-        }
+    public function connect() {
+      try {
+        $this->connection = new PDO(
+        "mysql:host=".DBConfig::$HOST.";dbname=".DBConfig::$DB, DBConfig::$USER,
+        DBConfig::$PWD);
+      } catch (Exception $ex) {
+        throw new Exception('Erro ao tentar se conectar.
+        Mensagem: '.$ex->getMessage());
+      }
     }
 
+    public function prepareStm($query, $fields) {
+      $stm = $this->connection->prepare($query);
+      foreach($fields as $key => &$value) {
+        $stm->bindParam(":".$key, $value);
+      }
+      return $stm;
+    }
+
+    public function executeQuery($stm) {
+      $isSelect = str_starts_with(strtolower($stm->queryString), "select");
+      $stm->execute();
+      $arr = $stm->fetchAll(PDO::FETCH_ASSOC);
+      $numRows = $stm->rowCount();
+      $stm = null;
+      if (!$isSelect) {
+        return $numRows;
+      }
+
+      return $arr;
+    }
+  }
 ?>
