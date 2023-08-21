748f016e57cfecc70c3700b75dbac316d7325e57
Samuel Filipe Faria <samuel.filipe@prevision.com.br>
2023-08-20 23:25:50 -0300

    feat(app): Creating login form

---
+++ front-end/src/components/LoginForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <v-sheet width="300" height="300" class="mx-auto">
+        <v-form @submit.prevent="submitUserForm">
+          <v-text-field
+            v-model="email"
+            :rules="rules"
+            label="E-mail"
+            type="email"
+          />
+          <v-text-field
+            v-model="password"
+            :rules="rules"
+            label="Senha"
+            type="password"
+          />
+          <v-btn type="submit" block class="mt-2">Entrar</v-btn>
+        </v-form>
+      </v-sheet>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    data: () => ({
+      email: '',
+      password: '',
+      rules: [ value => { return value ? true : 'Não é possível deixar campos vazios!' }, ],
+    }),
+    methods: {
+      submitUserForm() {
+        const requestOptions = {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        };
+
+        axios.post('http://localhost:8000/controller/UserController.class.php', this.prepareFormData, requestOptions)
+        .then(response => console.log(response.data) )
+        .catch(error => console.log('Ocorreu um erro: ' + error) );
+      }
+    },
+    computed: {
+      prepareFormData() {
+        let params = new URLSearchParams();
+        params.append('_acao', 'entrar');
+        params.append('email', this.email);
+        params.append('password', this.password);
+
+        return params;
+      }
+    }
+  }
+</script>
