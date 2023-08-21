7a4744159d8280a7e5db4225da28543d696be886
Samuel Filipe Faria <samuel.filipe@prevision.com.br>
2023-08-20 21:30:41 -0300

    feat(app): Creating utils for build response messages

---
+++ back-end/controller/Utils.class.php
@@ -0,0 +1,14 @@
+<?php
+  /**
+   * Builds a feedback JSON message
+   *
+   * @param string $msg Message description
+   * @param int $code Sucessfull message if code equals 1, 0 otherwise
+   * @return string JSON string format
+   */
+  class Utils {
+    public static function buildJSONMessage($msg, $code) {
+      return json_encode(array('message' => $msg, 'status_code' => $code));
+    }
+  }
+?>
